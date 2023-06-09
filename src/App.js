94a3f71ae45760b22934d8426c2cd5c82be8fcb1
SurajArya45 <131338563+SurajArya45@users.noreply.github.com>
2023-06-09 13:35:49 +0530

    initial commit

--- src/App.js
+++ src/App.js
@@ -8,6 +8,8 @@ import Login from './components/Login/login';
 import Home from './components/Home';
 import Navbar from './components/navbar/navbar';
 import 'bootstrap/dist/css/bootstrap.min.css';
+import CustomerDashboard from './components/customer/customerDashboard';
+import AdminDashboard from './components/admin/adminDashboard';
  
 function App() {
   return (
@@ -21,6 +23,9 @@ function App() {
           <Route path='/register' element = {<Register/>}/>
           <Route path='/aboutus' element = {<AboutUs/>}/>
           <Route path='/contactus' element = {<ContactUs/>}/>
+          <Route path='/CustomerDashboard' element = {<CustomerDashboard/>}/>
+          <Route path='/AdminDashboard' element = {<AdminDashboard/>}/>
+          
         </Routes> 
        
 
